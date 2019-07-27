efa19eeab44d13270d45b6d7c1c6db9e23f826d6
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-27 15:02:30 -0300

    Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function Main() {
-  return <View />;
+  return <Container />;
 }
 
 Main.navigationOptions = {
-  title: 'Usuários',
+  title: "Usuários"
 };
