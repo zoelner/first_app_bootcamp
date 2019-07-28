cae09f6778d940cb3cc6074b8fcc3ee9a73e7e09
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-28 01:27:47 -0300

    Loading e disabled

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Keyboard } from "react-native";
+import { Keyboard, ActivityIndicator } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import api from "../../services/api";
 
@@ -20,11 +20,14 @@ import {
 class Main extends Component {
   state = {
     newUser: "",
-    users: []
+    users: [],
+    loading: false
   };
 
   handleAddUser = async () => {
     const { newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -37,14 +40,15 @@ class Main extends Component {
 
     this.setState(prevState => ({
       users: [...prevState.users, data],
-      newUser: ""
+      newUser: "",
+      loading: false
     }));
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
 
     return (
       <Container>
@@ -58,8 +62,12 @@ class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
 
